91b4c4d24cc3719cc5c62046da1a9604d974c393
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-11-24 19:08:27 +0100

    [Actions] Updated CI action

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../LoggerKit"),
-        .package(path: "../CommandLineKit"),
-        .package(path: "../FoundationKit"),
-        .package(path: "../CoreGraphicsKit"),
+        .package(url: "https://github.com/pvieito/CommandLineKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/FoundationKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/CoreGraphicsKit.git", .branch("master")),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", .branch("master"))
     ],
     targets: [
---
+++ README.md
@@ -0,0 +1,13 @@
+# CollageKit
+
+Swift framework and tool to parse and render Picasa Collage files (.cxf).
+
+## Usage
+
+```bash
+$ swift run CollageTool -i CollageName.cxf
+```
+
+## Dependencies
+
+- Swift 5.1+
